6d44c14cf81df3004835e6d214f8ef5190ff2322
maru <lizmahrk@gmail.com>
2024-05-09 09:18:48 +0300

    change window name

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.setWindowTitle("CPReader");
     w.resize(900,500);
     w.setWindowIcon(QIcon(":icons/cpr.png"));
     w.show();
